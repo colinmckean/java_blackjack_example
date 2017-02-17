39fe42a3f87de8e64b1ea6134267b10dde2f4c75
Colin McKean <colinmckeandigital@gmail.com>
2017-02-17 21:03:12 +0000

    add Game constructor

---
+++ blackjack/Game.java
@@ -0,0 +1,13 @@
+package blackjack;
+import java.util.*;
+
+public class Game {
+  private ArrayList<Player> players;
+
+  public Game(){
+    //some inputs for names
+    //dealer, deck of cards.
+    players = new ArrayList<Player>();
+  }
+
+}
