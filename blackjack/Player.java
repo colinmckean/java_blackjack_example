3f29f17ab443d147fcb8d96026d01a408d2800c7
Colin McKean <colinmckeandigital@gmail.com>
2017-02-17 19:50:06 +0000

    add score calculator for Players hand.

--- blackjack/Player.java
+++ blackjack/Player.java
@@ -1,16 +1,25 @@
 package blackjack;
+import java.util.*;
 
 public class Player {
-  private name;
+  private String name;
   private ArrayList<Card> hand;
+  private int handValue;
 
   public Player(String name, Card card){
     this.hand = new ArrayList<Card>();
     hand.add(card);
     this.name = name;
   }
-  
+
   public String getName(){
     return this.name;
+  }
+
+  public int getHandValue(){
+    for(Card card : hand){
+      handValue += card.getCardValue().ordinal() + 2;
+    }
+    return handValue;
   }
 }
