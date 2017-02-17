216dfb170fcdf8d2e411f78c412cac8a659d8d16
Colin McKean <colinmckeandigital@gmail.com>
2017-02-17 19:08:30 +0000

    Test ordinal and name of card

--- specs/CardTest.java
+++ specs/CardTest.java
@@ -4,14 +4,21 @@ import blackjack.*;
 
 public class CardTest {
   Card card;
+  CardValue value;
 
   @Before
   public void before(){
-    card = new Card();
+    value = CardValue.values()[10];
+    card = new Card(value);
   }
 
   @Test
   public void testCardHasValue(){
-    assertEquals(10, card.getHandStrength());
+    assertEquals(10, card.getCardValue().ordinal());
+  }
+
+  @Test
+  public void testCardHasName(){
+    assertEquals("QUEEN", card.getCardValue().toString());
   }
 }
