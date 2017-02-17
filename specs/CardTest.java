e159d09d141da537de52e5f324a865e53ceeee08
Colin McKean <colinmckeandigital@gmail.com>
2017-02-17 19:42:56 +0000

    add tests to test player hands

--- specs/CardTest.java
+++ specs/CardTest.java
@@ -4,12 +4,20 @@ import blackjack.*;
 
 public class CardTest {
   Card card;
+  Card card_2;
   CardValue value;
+  CardValue value_2;
+  Player player_1;
+  Player player_2;
 
   @Before
   public void before(){
     value = CardValue.values()[0];
+    value_2 = CardValue.values()[1];
     card = new Card(value);
+    card_2 = new Card(value_2);
+    player_1 = new Player("Colin", card);
+    player_2 = new Player("Big Boss", card_2);
   }
 
   @Test
@@ -21,6 +29,12 @@ public class CardTest {
   @Test
   public void testCardHasName(){
     assertEquals("TWO", card.getCardValue().toString());
+  }
+
+  @Test
+  public void testPlayerHand(){
+    assertEquals(2, player_1.getHandValue());
+    assertEquals(3, player_2.getHandValue());
   }
 
 
