3357d06feef9076faf3db144817a0014a2e241a5
Colin McKean <colinmckeandigital@gmail.com>
2017-02-18 10:36:45 +0000

    add suit to Card class

--- blackjack/Card.java
+++ blackjack/Card.java
@@ -2,12 +2,17 @@ package blackjack;
 
 public class Card {
   private CardValue cardValue;
+  private Suit suit;
 
-  public Card(CardValue cardValue){
+  public Card(CardValue cardValue, Suit suit){
     this.cardValue = cardValue;
+    this.suit = suit;
   }
 
   public CardValue getCardValue(){
     return cardValue;
   }
+  public Suit getSuit(){
+     return suit;
+   }
 }
