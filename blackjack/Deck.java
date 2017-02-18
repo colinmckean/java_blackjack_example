72bd19b946151c832ba2140f770c35d71e803f3d
Colin McKean <colinmckeandigital@gmail.com>
2017-02-18 11:21:20 +0000

    add Deck, 52 cards, shuffled.

---
+++ blackjack/Deck.java
@@ -0,0 +1,25 @@
+package blackjack;
+
+import java.util.*;
+
+public class Deck{
+  private ArrayList<Card> deck;
+
+  public Deck(){
+    this.deck = new ArrayList<Card>();
+
+    for (int i = 0; i < 13; i++){
+      CardValue value = CardValue.values()[i];
+      for (int j=0; j < 4; j++){
+        Card card = new Card(value, Suit.values()[j]);
+        this.deck.add(card);
+      }
+    }
+    Collections.shuffle(deck);
+  }
+  
+  public int getDeckSize(){
+    return deck.size();
+  }
+
+}
