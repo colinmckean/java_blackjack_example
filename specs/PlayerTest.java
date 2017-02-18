0c4073bfb860853c922ad4e61981a5a05196c3e4
Colin McKean <colinmckeandigital@gmail.com>
2017-02-18 10:48:25 +0000

    add suit to show hand

--- specs/PlayerTest.java
+++ specs/PlayerTest.java
@@ -2,6 +2,7 @@ import static org.junit.Assert.*;
 import org.junit.*;
 import blackjack.*;
 public class PlayerTest {
+  
   Card card;
   Card card_2;
   Card card_3;
@@ -11,10 +12,15 @@ public class PlayerTest {
   Player player_1;
   Player player_2;
   Player player_3;
+  Suit suit;
+  Suit suit_2;
+  Suit suit_3;
+
   @Before 
   public void before(){
+    suit = Suit.values()[0];
     value = CardValue.values()[0];
-    card = new Card(value);
+    card = new Card(value, suit);
     player_1 = new Player("Colin", card);    
     player_2 = new Player("Chris", card);
   }
@@ -30,10 +36,10 @@ public class PlayerTest {
     player_1.calculateHandValue();
     assertEquals(2, player_1.getHandValue());
   }
-  
+
   @Test
   public void testRevealHand(){
     player_1.revealHand();
-    assertEquals("TWO", player_1.revealHand());
+    assertEquals("TWO OF HEARTS", player_1.revealHand());
   }
 }
