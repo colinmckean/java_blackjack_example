8e91fa8a4e54b2480af1e3815d62f85c7c21b557
Colin McKean <colinmckeandigital@gmail.com>
2017-02-18 10:11:13 +0000

    pass player tests

--- blackjack/Player.java
+++ blackjack/Player.java
@@ -5,21 +5,30 @@ public class Player {
   private String name;
   private ArrayList<Card> hand;
   private int handValue;
+  private String cardsInHand = "";
 
   public Player(String name, Card card){
     this.hand = new ArrayList<Card>();
     hand.add(card);
     this.name = name;
+    cardsInHand += card.getCardValue().toString();
   }
 
   public String getName(){
     return this.name;
   }
 
-  public int getHandValue(){
-    for(Card card : hand){
-      handValue += card.getCardValue().ordinal() + 2;
+  public void calculateHandValue(){
+    for(int i = 0; i < hand.size(); i++){
+      this.handValue += hand.get(0).getCardValue().ordinal() + 2;
     }
-    return handValue;
+  }
+
+  public int getHandValue(){
+    return this.handValue;
+  }
+
+  public String revealHand(){
+    return cardsInHand;
   }
 }
