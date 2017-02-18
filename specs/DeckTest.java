3eb84f08b60cae17bf4f4cfd798df5a6c1cc3e99
Colin McKean <colinmckeandigital@gmail.com>
2017-02-18 11:17:33 +0000

    add test to ensure I can get a deck

---
+++ specs/DeckTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import blackjack.*;
+
+public class DeckTest {
+  Deck deck;
+
+  @Before
+  public void before(){
+    deck = new Deck();
+  }
+  @Test
+  public void testDeckSize(){
+    assertEquals(52, deck.getDeckSize());
+  }
+}
