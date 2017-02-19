6474f3e1a88a7a475a041a5872317353189218d6
Colin McKean <colinmckeandigital@gmail.com>
2017-02-19 09:38:47 +0000

    pass tests that palyer can take card and refactor

--- blackjack/Player.java
+++ blackjack/Player.java
@@ -7,19 +7,24 @@ public class Player {
   private int handValue;
   private String cardsInHand = "";
 
+  public Player(String name){
+    this.hand = new ArrayList<Card>();
+    this.name = name;
+  }
   public Player(String name, Card card){
     this.hand = new ArrayList<Card>();
     hand.add(card);
     this.name = name;
     cardsInHand += card.getCardValue().toString();
     cardsInHand += " OF " + card.getSuit().toString();
-  }
+  }  
 
   public String getName(){
     return this.name;
   }
 
   public void calculateHandValue(){
+    handValue = 0;
     for(int i = 0; i < hand.size(); i++){
       this.handValue += hand.get(0).getCardValue().ordinal() + 2;
     }
@@ -32,4 +37,9 @@ public class Player {
   public String revealHand(){
     return cardsInHand;
   }
+  public void takeCard(Card card){
+    hand.add(card);
+    calculateHandValue();
+  }
+
 }
