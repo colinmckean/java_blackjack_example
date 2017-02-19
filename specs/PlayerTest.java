eb00b5a3893931b442092442df61063891b9cfe0
Colin McKean <colinmckeandigital@gmail.com>
2017-02-19 19:41:53 +0000

    add test to check for blackjack values

--- specs/PlayerTest.java
+++ specs/PlayerTest.java
@@ -3,29 +3,41 @@ import org.junit.*;
 import blackjack.*;
 public class PlayerTest {
   
-  Card card;
-  Card card_2;
-  Card card_3;
-  CardValue value;
-  CardValue value_2;
-  CardValue value_3;
+  CardValue two;
+  CardValue king;
+  CardValue ace;
+
+  Suit hearts;
+  Suit spades;
+  Suit clubs;
+  Suit diamonds;
+  
+
+  Card queenOfHearts;
+  Card twoOfSpades;
+  Card kingOfHearts;
+  Card aceOfSpades;
+
   Player player_1;
   Player player_2;
   Player player_3;
-  Suit suit;
-  Suit suit_2;
-  Suit suit_3;
 
   @Before 
   public void before(){
-    suit = Suit.values()[0];
-    suit_2 = Suit.values()[1];
-    value_2 = CardValue.values()[0];
-    value = CardValue.values()[0];
-    card = new Card(value, suit);
-    card_2 = new Card(value_2, suit_2);
-    player_1 = new Player("Colin", card);    
-    player_2 = new Player("Chris", card);
+    hearts = Suit.values()[0];
+    spades = Suit.values()[1];
+
+    two = CardValue.values()[0];
+    king = CardValue.values()[11];
+    ace = CardValue.values()[12];
+
+    queenOfHearts = new Card(value, hearts);
+    twoOfSpades = new Card(two, spades);
+    kingOfHearts = new Card(king, hearts);
+    aceOfSpades = new Card(ace, spades);
+
+    player_1 = new Player("Colin", queenOfHearts);
+    player_2 = new Player("Chris", queenOfHearts);
     player_3 = new Player("John");
   }
 
@@ -38,13 +50,13 @@ public class PlayerTest {
   @Test 
   public void testCalculateHandValue(){
     player_1.calculateHandValue();
-    assertEquals(2, player_1.getHandValue());
+    assertEquals(12, player_1.getHandValue());
   }
 
   @Test
   public void testRevealHand(){
     player_1.revealHand();
-    assertEquals("TWO OF HEARTS", player_1.revealHand());
+    assertEquals("QUEEN OF HEARTS", player_1.revealHand());
   }
 
   @Test 
@@ -52,12 +64,17 @@ public class PlayerTest {
     player_3.calculateHandValue();
     assertEquals(0, player_3.getHandValue());
 
-    player_3.takeCard(card_2);
+    player_3.takeCard(twoOfSpades);
     assertEquals(2, player_3.getHandValue());
 
-    player_3.takeCard(card);
-    assertEquals(4, player_3.getHandValue());
+    player_3.takeCard(queenOfHearts);
+    assertEquals(14, player_3.getHandValue());
+  }
 
-
+  @Test 
+  public void testBlackjackValues(){
+    player_3.takeCard(kingOfHearts);
+    player_3.takeCard(aceOfSpades);
+    assertEquals(21, player_3.getBlackjackValue());
   }
 }
