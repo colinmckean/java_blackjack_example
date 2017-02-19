90d333cedfb98fa192624245310e2186f9fec7a8
Colin McKean <colinmckeandigital@gmail.com>
2017-02-19 19:44:03 +0000

    tidy up variable names and correct typos

--- specs/CardTest.java
+++ specs/CardTest.java
@@ -3,49 +3,56 @@ import org.junit.*;
 import blackjack.*;
 
 public class CardTest {
-  Card card;
-  Card card_2;
-  Card card_3;
-  CardValue value;
-  CardValue value_2;
-  CardValue value_3;
-  Player player_1;
-  Player player_2;
-  Player player_3;
-  Suit suit;
-  Suit suit_2;
-  Suit suit_3;
+  Card twoOfHearts;
+  Card aceOfSpades;
+  Card jackOfClubs;
+
+  CardValue two;
+  CardValue five;
+  CardValue ace;
+  CardValue jack;
+
+  Suit hearts;
+  Suit spades;
+  Suit clubs;
+  Suit diamonds;
 
   @Before
   public void before(){
-    value = CardValue.values()[0];
-    suit = Suit.values()[0];
-    value_2 = CardValue.values()[1];
-    suit_2 = Suit.values()[1];
-    value_3 = CardValue.values()[3];
-    suit_3 = Suit.values()[2];
-    card = new Card(value, suit);
-    card_2 = new Card(value_2, suit_2);
-    card_3 = new Card(value_3, suit_3);
+    two = CardValue.values()[0];
+    five = CardValue.values()[3];
+    jack = CardValue.values()[9];
+    ace = CardValue.values()[12];
+    hearts = Suit.values()[0];
+    spades = Suit.values()[1];
+    clubs = Suit.values()[2];
+    diamonds = Suit.values()[3];
+    twoOfHearts = new Card(two, hearts);
+    aceOfSpades = new Card(ace,spades);
+    jackOfClubs = new Card(jack, clubs);
   }
 
   @Test
   public void testCardHasValue(){
     // + 2;
-    assertEquals(0, card.getCardValue().ordinal());
+    assertEquals(0, twoOfHearts.getCardValue().ordinal());
   }
 
   @Test
   public void testCardHasName(){
-    assertEquals("TWO", card.getCardValue().toString());
+    assertEquals("TWO", twoOfHearts.getCardValue().toString());
+    assertEquals("JACK", jackOfClubs.getCardValue().toString());
   }
+  
   @Test
   public void testGetSuit(){
-    assertEquals("HEARTS", card.getSuit().toString());
-    assertEquals("SPADES", card_2.getSuit().toString());
-    assertEquals("CLUBS", card_3.getSuit().toString());
+    assertEquals("HEARTS", twoOfHearts.getSuit().toString());
+    assertEquals("SPADES", aceOfSpades.getSuit().toString());
+    assertEquals("CLUBS", jackOfClubs.getSuit().toString());
   }
+
+  @Test
   public void testGetFriendlyName(){
-    assertEquals("TWO OF HEARTS", card.getFriendlyName());
+    assertEquals("\nTWO OF HEARTS", twoOfHearts.getFriendlyName());
   }
 }
